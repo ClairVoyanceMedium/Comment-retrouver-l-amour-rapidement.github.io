431779071e850e64b582a0924af1cae81b91002b
ClairVoyanceMedium <contact.clairvoyancemedium@gmail.com>
2025-01-31 22:28:13 +0100

    Delete update-year.js

--- update-year.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var yearElements = document.querySelectorAll("#current-year");
-    yearElements.forEach(el => el.textContent = new Date().getFullYear());
-});
